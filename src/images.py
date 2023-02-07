ab72ecc55d5494686bb31e01a29578490b516ff9
Giadissima <52421987+Giadissima@users.noreply.github.com>
2023-02-07 10:42:45 +0100

    Piccole modifiche in isImages

--- src/images.py
+++ src/images.py
@@ -1,12 +1,8 @@
 from PIL import Image, UnidentifiedImageError
 def isImage(img):
   try:
-    image = Image.open(img)
-    image.verify()
+    with Image.open(img) as image:
+      image.verify()
     return True
-  except FileNotFoundError:
-    raise FileNotFoundError("file non trovato")
   except UnidentifiedImageError:
-    return False    
-  except Exception:
     return False
