0e64e567d42eb5a25101e4ad228a68640c8ff4f9
Giadissima <52421987+Giadissima@users.noreply.github.com>
2023-10-20 19:30:52 +0200

    added icon on tkinter frame

--- test/prova_interfaccia.py
+++ test/prova_interfaccia.py
@@ -1,9 +1,19 @@
 from tkinter import *
 from tkinter import ttk
+from PIL import Image
+
 root = Tk()
 root.title("Sort Image")
-root.geometry("500x500")
-ttk.Label(root, text="Hello World!").pack()
-btn = ttk.Button(root, text="Quit", command=root.destroy)
+root.geometry("600x700")
+root.configure(bg="red")
+root.iconbitmap('icon.ico')
+title = ttk.Label(root, text="Sort Image", font='Helvetica 20 bold', padding=10)
+title.pack()
+
+optionFrame = ttk.Frame(root, padding=10)
+
+print(ttk.Frame(root, padding=10).configure())
+
+btn = ttk.Button(root, text="Inizia", command=root.destroy)
 btn.pack()
 root.mainloop()
