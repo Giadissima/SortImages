bca1c0eccb8e77c86bd38d5233b8f48d8878d880
Giadissima <52421987+Giadissima@users.noreply.github.com>
2023-12-20 16:05:14 +0100

    added docstring

--- meta/decorators.py
+++ meta/decorators.py
@@ -1,11 +1,12 @@
-# decorators.py
-
 def singleton(cls):
-    instances = {}
+  """define a decorator capable of generating a single instance of the specified class at a time
+  Args:
+  cls: The class to which the decorator is applied."""
+  instances = {}
 
-    def get_instance(*args, **kwargs):
-        if cls not in instances:
-            instances[cls] = cls(*args, **kwargs)
-        return instances[cls]
+  def get_instance(*args, **kwargs):
+    if cls not in instances:
+      instances[cls] = cls(*args, **kwargs)
+    return instances[cls]
 
-    return get_instance
+  return get_instance
