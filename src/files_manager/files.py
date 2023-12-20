9a474fa89bf6e01761da0102a64d14dd1a5a7851
Giadissima <52421987+Giadissima@users.noreply.github.com>
2023-12-20 16:47:44 +0100

    created function check_file_name and added docstring

--- src/files_manager/files.py
+++ src/files_manager/files.py
@@ -13,17 +13,14 @@ class File:
     self.HASH_LIST = set()
     
   def move_file(self, file_path: str, file_name: str, new_path: str):
-    # print(current_path, file, new_path)
+    """Move a file in a new directory, and in case of name duplicate error, rename the file.
+    Args:
+      file_path (string): old file's path,
+      file_name(string): name of the file
+      new_path (string): new file's path
+    """
     Folder.create_nested_dir(new_path)
-    # cambia il nome del file in modo univoco
-    file_dest_path = join(new_path, file_name)
-    i = 0
-    file_list = file_name.rsplit('.', 1)
-    
-    while exists(file_dest_path):
-      file_name = f"{file_list[0]}.{i}.{file_list[1]}"
-      file_dest_path = join(new_path, file_name) 
-      i+=1
+    file_dest_path = self.check_file_name(file_name)
     try:
       rename(file_path, file_dest_path)
       return True
@@ -54,11 +51,24 @@ class File:
     return hash.hexdigest()
   
   @classmethod
-  def extract_date(cls, file_path: str, file: str, folder_date: List[str]|None):
+  def extract_date(cls, file_path: str, file: str, folder_date: List[str]|None)->[str, str, str]:
+    """Extract date from metadata and file_name, giving priority in order to:
+      - folder_date of lenght 3,
+      - metadata (date of creation)
+      - file name
+      - folder_date of other lengths
+
+    Args:
+        cls (ImageHelper|VideoHelper): class to which a file belongs which can be an image or a video
+        file_path (str): where file's stored
+        file (str): name of the file
+        folder_date (List[str] | None): the date from the folder
+
+    Returns:
+        [year(str), month(str), day(str)]: date extracted from file and folder
+    """
     regex = RegexMedia()
     if(folder_date != None and len(folder_date) == 3): 
-      # print(folder_date)
-      # print(file_path, "founded folder date")
       return folder_date
     date = cls.get_date_from_metadata(file_path)
     # se la data non era contenuta nei metadati allora guardo se la trovo nel nome del media
@@ -73,7 +83,7 @@ class File:
     the preferences previously entered by the user.
     Args:
       file_path (str): the entire path of the file to check
-      file:name (str): just the name of the file to check
+      file_name (str): just the name of the file to check
     Returns:
       Union[bool, str|None]: 
         bool: True if file is a duplicate, False otherwise
@@ -87,8 +97,18 @@ class File:
         return True, f'{file_name} - Duplicated detected: file not moved.'
     return False, None
   
-  def handle_move_file(self, media_class, file_path:str, file_name:str, folder_date: str):
-    """Checks if the passed date is valid, and if so, creates the new path for the image and moves it."""
+  def handle_move_file(self, media_class, file_path:str, file_name:str, folder_date: str)-> Union[str, str]:
+    """Checks if the passed date is valid, and if so, creates the new path for the image and moves it.
+
+    Args:
+        media_class (ImageHelper|VideoHelper): class to which a file belongs which can be an image or a video
+        file_path (str): the entire path of the file
+        file_name (str): the name of the file
+        folder_date (List[str] | None): date extracted from folder
+
+    Returns:
+        Union[function_name(str), message(str)]: log message with a default value stored in function_name with a message
+    """
     date = media_class.extract_date(file_path, file_name, folder_date)
     
     # case no date found: file not moved
@@ -102,3 +122,24 @@ class File:
       return 'debug', f'{file_name} - moved successfully.'
     else: 
       return 'error', f'{file_name} - The file could not be moved'
+    
+  def check_file_name(self, file_name, new_path) -> str:
+    """Check if file name already exists in destination path, if so, 
+    find another name and return it
+    
+    Args:
+    file_name(str): the name of the file
+    new_path(str): the path to whitch move the file
+    
+    Return:
+    str: the new path with name checked
+    """
+    file_dest_path = join(new_path, file_name)
+    file_list = file_name.rsplit('.', 1)
+    
+    i = 0
+    while exists(file_dest_path):
+      file_name = f"{file_list[0]}.{i}.{file_list[1]}"
+      file_dest_path = join(new_path, file_name) 
+      i+=1
+    return file_dest_path
